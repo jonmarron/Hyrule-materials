7117acaa5fe84faefebf4014f5920d9ef6dc9aaa
Jon Ladron de Guevara <jldeg@hotmail.com>
2023-03-19 22:18:00 +0100

    Hide sorting buttons when seeing more info, added the fav btn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,21 +12,48 @@ const getMaterials = async (setMaterials) => {
 
 function App() {
   const [ materials, setMaterials ] = useState(null);
-  const [favMaterials, setFavMaterials] = useState(null)
+  const [ favMaterials, setFavMaterials ] = useState(null)
+  const [ sortAscending, setSortAscending ] = useState(null);
+  const [ hideSort, setHideSort ] = useState(false);
+
+  // is there a better way to do this? probably yes
+  const [ learnMoreCounter, setLearnMoreCounter ] = useState(0);
 
   useEffect(()=> {
     getMaterials(setMaterials)
   }, [])
+  
+  useEffect(() => {
 
+    // I want the materials to be sorted ascending since the beginning. It only works if I set "materials" as a dependency of this useEffect, but that means it re-renders everytime and it is an infinte loop, how do i do this better without repeating this funtcion when setting the materials first after fetching
+    if (materials && sortAscending) {
+      const sortedMaterials = [...materials].sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      })
+      setMaterials(sortedMaterials);
+    } else if (materials && !sortAscending) {
+      const sortedMaterials = [...materials].sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      })
+      setMaterials(sortedMaterials);
+    }
+  }, [sortAscending])
+  
   return (
     <div className="App">
       <h1 id="site-name">Hyrule Materials</h1>
       <div id='filter-box'>
-        <button>Sort Ascending</button>
-        <button>Sort Descending</button>
+        {hideSort? (
+          <></>
+        ) : (
+          <>
+            <button onClick = { e => setSortAscending(true)}>Sort Ascending</button>
+            <button onClick = { e => setSortAscending(false)}>Sort Descending</button>
+          </>
+        )}
       </div>
       <div id="cards-container">
-        {console.log(materials)}
+
         {materials && materials.map((material, index) => {
           return (
             <MaterialCard
@@ -34,6 +61,9 @@ function App() {
               name = { material.name}
               image = { material.image}
               description = { material.description}
+              setHideSort = { setHideSort }
+              setLearnMoreCounter = { setLearnMoreCounter }
+              learnMoreCounter = { learnMoreCounter }
             />
           )
         })}
--- client/src/components/MaterialCard.jsx
+++ client/src/components/MaterialCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const MaterialCard = ({name, image, description}) => {
+export const MaterialCard = ({name, image, description, setHideSort, setLearnMoreCounter, learnMoreCounter}) => {
+
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
     <div className='materialCard'>
+      <button className="fav-btn">+</button>
       <img src={image} className='materialCardImage'/>
       <p className="material-name">{name}</p>
-      <p className="material-descr">{description}</p>
+      {showInfo ? (
+        <>
+          <p className="material-descr">{description}</p>
+          <button onClick = { e => {
+            setShowInfo(!showInfo);
+            setLearnMoreCounter(learnMoreCounter - 1)
+            if(learnMoreCounter === 1 ) setHideSort(false);
+          }}>Show less</button>
+        </>
+      ):(
+        <button onClick = { e => {
+          setShowInfo(!showInfo);
+          setHideSort(true);
+          setLearnMoreCounter(learnMoreCounter + 1)
+        }}>Show more</button>
+      )}
     </div>
   )
 }
