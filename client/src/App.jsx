eac17d41eddf838c20b75643565b009362b53be7
Jon Ladron de Guevara <jldeg@hotmail.com>
2023-03-22 06:39:29 +0100

    Added the search function to work also when deleting characters

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,18 +11,26 @@ const getMaterials = async (setMaterials) => {
 }
 
 function App() {
-  const [ materials, setMaterials ] = useState(null);
-  const [ favMaterials, setFavMaterials ] = useState(null)
-  const [ sortAscending, setSortAscending ] = useState(null);
-  const [ hideSort, setHideSort ] = useState(false);
+  const [materials, setMaterials] = useState([]);
+  const [favMaterials, setFavMaterials] = useState(null)
+  const [sortAscending, setSortAscending] = useState(null);
+  const [hideSort, setHideSort] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  // is there a better way to do this? probably yes
-  const [ learnMoreCounter, setLearnMoreCounter ] = useState(0);
+  // is there a better way to do this? 👇 (( probably yes 😅))
+  const [learnMoreCounter, setLearnMoreCounter] = useState(0);
 
-  useEffect(()=> {
-    getMaterials(setMaterials)
+  useEffect(() => {
+    getMaterials(setMaterials);
+
+    // 👇 this throws an error saying that 'materials' is not iterable, what leads me to believe that the materials are not really set yet...
+    const sortedMaterials = [...materials].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    })
+    console.log(sortedMaterials)
+    setMaterials(sortedMaterials);
   }, [])
-  
+
   useEffect(() => {
 
     // I want the materials to be sorted ascending since the beginning. It only works if I set "materials" as a dependency of this useEffect, but that means it re-renders everytime and it is an infinte loop, how do i do this better without repeating this funtcion when setting the materials first after fetching
@@ -38,35 +46,41 @@ function App() {
       setMaterials(sortedMaterials);
     }
   }, [sortAscending])
-  
+
+  const handleSearch = event => {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1 id="site-name">Hyrule Materials</h1>
       <div id='filter-box'>
-        {hideSort? (
+        <input type="text" name="mat-search" placeholder='Search' onChange={handleSearch} />
+        {hideSort ? (
           <></>
         ) : (
           <>
-            <button onClick = { e => setSortAscending(true)}>Sort Ascending</button>
-            <button onClick = { e => setSortAscending(false)}>Sort Descending</button>
+            <button onClick={e => setSortAscending(true)}>Sort Ascending</button>
+            <button onClick={e => setSortAscending(false)}>Sort Descending</button>
           </>
         )}
       </div>
       <div id="cards-container">
 
-        {materials && materials.map((material, index) => {
-          return (
-            <MaterialCard
-              key = { index }
-              name = { material.name}
-              image = { material.image}
-              description = { material.description}
-              setHideSort = { setHideSort }
-              setLearnMoreCounter = { setLearnMoreCounter }
-              learnMoreCounter = { learnMoreCounter }
-            />
-          )
-        })}
+        {materials.filter(material => material.name.toLowerCase().includes(searchQuery.toLowerCase()))
+          .map((material, index) => {
+            return (
+              <MaterialCard
+                key={index}
+                name={material.name}
+                image={material.image}
+                description={material.description}
+                setHideSort={setHideSort}
+                setLearnMoreCounter={setLearnMoreCounter}
+                learnMoreCounter={learnMoreCounter}
+              />
+            )
+          })}
       </div>
     </div>
   )
